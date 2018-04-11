f5d045d71a859d13473e7c85c4d0e32e8ebfbdaa
super-cooper <adamcooperrc@gmail.com>
2018-04-11 03:56:02 -0400

    Define training routine

--- deepfly.py
+++ deepfly.py
@@ -121,13 +121,13 @@ n_outputs = len(labels_small_onehot[0])
 train_data, test_data = train_test_split(data_prepared, test_size=0.2)
 
 # Input tensor
-X = tf.placeholder(tf.float64, shape=(None, n_inputs), name='X')
+X = tf.placeholder(tf.float32, shape=(None, n_inputs), name='X')
 
 # label tensor
 y = tf.placeholder(tf.int32, shape=None, name='y')
 
 # This variable will tell batch norm if we're training or not
-training = tf.placeholder_with_default(False, shape=(), name='training?')
+training = tf.placeholder_with_default(False, shape=(), name='is_training')
 
 # We'll use He initialization for our weights
 he_init = tf.variance_scaling_initializer()
@@ -135,15 +135,100 @@ he_init = tf.variance_scaling_initializer()
 # Primary architecture
 with tf.name_scope("FlyNet"):
     bn_layer = partial(tf.layers.batch_normalization, training=training, momentum=0.9)
-    dense = partial(tf.layers.dense, kernel_initialization=he_init)
+    dense = partial(tf.layers.dense, kernel_initializer=he_init)
     hidden_1 = dense(X, n_hidden1, name='Hidden1')
     act_1 = tf.nn.selu(bn_layer(hidden_1))
     hidden_2 = dense(act_1, n_hidden2, name='Hidden2')
     act_2 = tf.nn.selu(bn_layer(hidden_2))
     logits = bn_layer(dense(act_2, n_outputs, name='Output'))
 
+# Defining loss function
 with tf.name_scope('loss'):
     x_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
     loss = tf.reduce_mean(x_entropy, name='loss')
     loss_summary = tf.summary.scalar('log_loss', loss)
 
+# Defining back-prop function
+learning_rate = 0.001
+with tf.name_scope('train'):
+    optimizer = tf.train.AdamOptimizer(learning_rate)
+    training_op = optimizer.minimize(loss)
+
+# Define evaluation function
+with tf.name_scope('eval'):
+    correct = tf.nn.in_top_k(logits, y, 1)
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+    accuracy_summary = tf.summary.scalar('accuracy', accuracy)
+
+# This is a utility to save all of the above information
+saver = tf.train.Saver()
+
+# Define training parameters
+m, n = train_data.shape
+n_epochs = 10000
+batch_size = 200
+n_batches = int(np.ceil(m / batch_size))
+
+# Some logging paths
+checkpoint_path = "/tmp/deep_fly_model.ckpt"
+checkpoint_epoch_path = checkpoint_path + ".epoch"
+final_model_path = "./deep_fly_model"
+file_writer = tf.summary.FileWriter(log_dir('mlb_dnn'), tf.get_default_graph())
+
+# Starting values for early stopping
+best_loss = np.inf
+epochs_without_progress = 0
+max_epochs_without_progress = 50
+
+# Explicit ops for batch normalization
+extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+
+# Now let's run it!
+with tf.Session() as sess:
+    if os.path.isfile(checkpoint_epoch_path):
+        # if the checkpoint file exists, restore the model and load the epoch number
+        with open(checkpoint_epoch_path, "rb") as f:
+            start_epoch = int(f.read())
+        print("Training was interrupted. Continuing at epoch", start_epoch)
+        saver.restore(sess, checkpoint_path)
+    else:
+        start_epoch = 0
+        tf.global_variables_initializer().run()
+
+    X_batch, y_batch = None, None
+    for epoch in range(n_epochs):
+        # Iterate through a number of mini-batches (enough to cover the whole training set)
+        for iteration in range(n_batches):
+            # Get our next batch of training data and targets
+            indices = np.random.choice(data_prepared.shape[0], batch_size, replace=False)
+            X_batch, y_batch = data_prepared[indices], labels_small_onehot[indices]
+            # Run the network on the batch
+            sess.run([training_op, extra_update_ops], feed_dict={X: X_batch, y: y_batch, training: True})
+            accuracy_val, loss_val, accuracy_summary_str, loss_summary_str = sess.run(
+                [accuracy, loss, accuracy_summary, loss_summary],
+                feed_dict={X: test_data, y: test_labels})
+            file_writer.add_summary(accuracy_summary_str, epoch)
+            file_writer.add_summary(loss_summary_str, epoch)
+
+            if epoch % 5 == 0:
+                print("Epoch:", epoch,
+                      "\tTest accuracy: {:.3f}%".format(accuracy_val * 100),
+                      "\tLoss: {:.5f}".format(loss_val))
+                saver.save(sess, checkpoint_path)
+                with open(checkpoint_epoch_path, "wb") as f:
+                    f.write(b"%d" % (epoch + 1))
+                if loss_val < best_loss:
+                    saver.save(sess, final_model_path)
+                    best_loss = loss_val
+                else:
+                    epochs_without_progress += 5
+                    if epochs_without_progress > max_epochs_without_progress:
+                        print("Early stopping")
+                        break
+
+os.remove(checkpoint_epoch_path)
+with tf.Session() as sess:
+    saver.restore(sess, final_model_path)
+    accuracy_val = accuracy.eval(feed_dict={X: test_data, y: test_labels, training: False})
+print('Best model accuracy:', accuracy_val)
+file_writer.close()

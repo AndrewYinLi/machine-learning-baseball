b098d5702903b90eb91b98705dcb609174a98d2a
super-cooper <adamcooperrc@gmail.com>
2018-04-11 03:22:39 -0400

    Set up neural network architecture

--- deepfly.py
+++ deepfly.py
@@ -1,15 +1,22 @@
 import os
 from datetime import datetime
+from functools import partial
 
 import numpy as np
 import pandas as pd
 import sqlalchemy
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.preprocessing import Imputer, StandardScaler, LabelBinarizer
 
 from utils import DataFrameSelector, CategoricalEncoder
 
+# "Global" variables
 verbose = True
+n_entries = 10000
+n_hidden1 = 300
+n_hidden2 = 100
 
 # import data with sqlalchemy
 db = "raw2017.db"
@@ -64,7 +71,7 @@ for attr in discrete_attrs:
 if os.path.exists("entries.npy"):
     entries = np.load("entries.npy")
 else:
-    entries = np.random.choice(data.shape[0], 10000, replace=False)
+    entries = np.random.choice(data.shape[0], n_entries, replace=False)
     np.save("entries.npy", entries)
 labels = np.array(data['events'])
 del data['events']
@@ -105,3 +112,38 @@ if verbose:
     print("Data prepared.")
 
 labels_small_onehot = LabelBinarizer().fit_transform(labels_small)
+
+# Deep Learning hyperparameters
+n_inputs = len(data_prepared[0])
+n_outputs = len(labels_small_onehot[0])
+
+# Split into training/testing data
+train_data, test_data = train_test_split(data_prepared, test_size=0.2)
+
+# Input tensor
+X = tf.placeholder(tf.float64, shape=(None, n_inputs), name='X')
+
+# label tensor
+y = tf.placeholder(tf.int32, shape=None, name='y')
+
+# This variable will tell batch norm if we're training or not
+training = tf.placeholder_with_default(False, shape=(), name='training?')
+
+# We'll use He initialization for our weights
+he_init = tf.variance_scaling_initializer()
+
+# Primary architecture
+with tf.name_scope("FlyNet"):
+    bn_layer = partial(tf.layers.batch_normalization, training=training, momentum=0.9)
+    dense = partial(tf.layers.dense, kernel_initialization=he_init)
+    hidden_1 = dense(X, n_hidden1, name='Hidden1')
+    act_1 = tf.nn.selu(bn_layer(hidden_1))
+    hidden_2 = dense(act_1, n_hidden2, name='Hidden2')
+    act_2 = tf.nn.selu(bn_layer(hidden_2))
+    logits = bn_layer(dense(act_2, n_outputs, name='Output'))
+
+with tf.name_scope('loss'):
+    x_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
+    loss = tf.reduce_mean(x_entropy, name='loss')
+    loss_summary = tf.summary.scalar('log_loss', loss)
+
